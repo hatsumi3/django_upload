f2b6bbda760d82afcf1ec1e2016f84ae2ccbc3bd
hatsumi3 <al.usamiku39@gmail.com>
2020-08-31 15:59:06 +0900

    add document model admin

--- ai_image/admin.py
+++ ai_image/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
+from django.utils import timezone
 from .models import Document
 # Register your models here.
+
+from django.db.models.signals import pre_save
+from django.dispatch import receiver
 
 class DocumentAdmin(admin.ModelAdmin):
     """Django 管理画面で表示する内容を設定するクラス
@@ -10,8 +14,12 @@ class DocumentAdmin(admin.ModelAdmin):
         admin ([type]): [description]
     """
     list_display = ('id', 'photo', 'uploaded_at', 'description')
-    read_only_fields = ('uploaded_at',)
+    readonly_fields = ('uploaded_at',)
     ordering = ('-uploaded_at',)
     search_fields = ('description',)
+
+@receiver(pre_save, sender=Document)
+def my_handler(sender, instance, **kwargs):
+    instance.uploaded_at = timezone.now()
 
 admin.site.register(Document, DocumentAdmin)
